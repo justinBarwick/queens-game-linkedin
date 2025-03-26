37f0c18538be33fbcebd6d0cfe67d969d83c9f6a
justinBarwick <53281261+justinBarwick@users.noreply.github.com>
2025-03-25 20:37:19 -0700

    Create neli1.js

---
+++ src/utils/levels/neli1.js
@@ -0,0 +1,120 @@
+
+import { altoMain, anakiwa, bittersweet, celadon, chardonnay, lightOrchid, lightWisteria, nomad, saharaSand } from "../colors";
+
+const levelNeli1 = {
+  size: 9,
+  colorRegions: [
+  [
+    "A",
+    "A",
+    "G",
+    "G",
+    "H",
+    "C",
+    "C",
+    "C",
+    "C"
+  ],
+  [
+    "B",
+    "A",
+    "A",
+    "G",
+    "H",
+    "H",
+    "H",
+    "H",
+    "H"
+  ],
+  [
+    "B",
+    "B",
+    "A",
+    "A",
+    "H",
+    "H",
+    "H",
+    "H",
+    "H"
+  ],
+  [
+    "B",
+    "B",
+    "F",
+    "A",
+    "A",
+    "G",
+    "G",
+    "I",
+    "I"
+  ],
+  [
+    "B",
+    "B",
+    "F",
+    "F",
+    "A",
+    "A",
+    "G",
+    "I",
+    "I"
+  ],
+  [
+    "B",
+    "B",
+    "F",
+    "F",
+    "F",
+    "A",
+    "A",
+    "H",
+    "H"
+  ],
+  [
+    "B",
+    "D",
+    "D",
+    "D",
+    "D",
+    "D",
+    "A",
+    "A",
+    "H"
+  ],
+  [
+    "B",
+    "D",
+    "D",
+    "D",
+    "E",
+    "E",
+    "E",
+    "A",
+    "A"
+  ],
+  [
+    "C",
+    "C",
+    "C",
+    "D",
+    "E",
+    "E",
+    "E",
+    "E",
+    "A"
+  ]
+],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+    I: lightOrchid
+  }
+};
+
+export default levelNeli1;
